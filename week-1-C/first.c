d9b97658ff25327a31d267ee37ae7994f93d5e0b
LocalRapid <sukrish14369@gmail.com>
2026-09-07 01:25:42 +0530

    week 1 just starting

---
+++ week-1-C/first.c
@@ -0,0 +1,12 @@
+// PS C:\Users\sukri\Documents\GitFirst\CS50\week-1-C> ./first.exe
+// hello, world
+// PS C:\Users\sukri\Documents\GitFirst\CS50\week-1-C> cd ..
+// PS C:\Users\sukri\Documents\GitFirst\CS50> ./week-1-c/first.exe
+// hello, world
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello World, I am learning C,\"Hi to quote\"\n");
+}
--- week-1-C/time-stamp-of-complete-w1.md
+++ week-1-C/time-stamp-of-complete-w1.md
@@ -2,7 +2,7 @@ Week 1 C programing
 
 dd/mm/yyyy
 Date: 06/09/2026-12am
-    1st completed 
+    1st break at 06/09/2026-01:30am timestamp: 24:20
     
 
 
